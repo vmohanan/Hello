b46b2803d4eda878ea8c83526ecfc9be455ce88e
Vinod <Vinod.Mohanan@gmail.com>
2015-12-16 21:44:31 -0500

    added add tests

--- src/test/java/mathtests.java
+++ src/test/java/mathtests.java
@@ -1,10 +1,26 @@
-/**
- * Created by vxmohana on 12/16/15.
- */
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.test;
-
+import quickstart.mathclass;
 
 public class mathtests {
-    
+    @Test
+    public void AddTest()
+    {
+        mathclass mc=new mathclass();
+        assertEquals(mc.Add(4,5),9);
+        assertEquals(mc.Add(0,5),5);
+        assertEquals(mc.Add(0,0),0);
+    }
+
+    @Test
+    public void AddNegativeTest()
+    {
+        mathclass mc=new mathclass();
+        assertEquals(mc.Add(-4,5),1);
+        assertEquals(mc.Add(0,-5),-5);
+        assertEquals(mc.Add(-1,-2),-3);
+        assertEquals(mc.Add(-1,1),0);
+    }
+
 }
