f77c55b35ded04266300c4a27d4f720978651464
Vinod <Vinod.Mohanan@gmail.com>
2015-12-21 15:18:08 -0500

    Added pmd in pom

--- src/main/java/quickstart/MathClass.java
+++ src/main/java/quickstart/MathClass.java
@@ -6,7 +6,15 @@ public class MathClass {
 
     public int Add(int Num1,int Num2)
     {
-        return Num1+Num2;
+        int Nu=0;
+        try {
+            Nu= Num1 + Num2;
+        }
+        catch (NumberFormatException nfe)
+        {
+
+        }
+        return Nu;
     }
 
     public long Multiply(int Num1,int Num2)
