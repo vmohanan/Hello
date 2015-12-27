a56052f680e588943f51fcdba85cd3cdf5db82aa
Vinod <Vinod.Mohanan@gmail.com>
2015-12-27 17:03:47 -0500

    Added an empty try catch block to multiple function

--- src/main/java/quickstart/MathClass.java
+++ src/main/java/quickstart/MathClass.java
@@ -18,7 +18,14 @@ public class MathClass {
 
     public long Multiply(int Num1,int Num2)
     {
-        return Num1*Num2;
+        long MVal1;
+        try {
+            MVal1= Num1 * Num2;
+        }
+            catch (NullPointerException nullPointerException)
+            {
+            }
+        return MVal1;
     }
 
     public float Divide(int Num1,int Num2)
